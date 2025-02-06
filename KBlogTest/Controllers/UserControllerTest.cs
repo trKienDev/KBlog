796ae35f7af799b6042558b92051ae292921ece6
trKienDev <trkien.dev@gmail.com>
2025-02-06 18:48:00 +0700

    Complete RESTful API of UserController & build unit test - KBlogTest

---
+++ KBlogTest/Controllers/UserControllerTest.cs
@@ -0,0 +1,63 @@
+﻿#pragma warning disable CS702
+
+using KBlog.Controllers;
+using KBlog.Data;
+using KBlog.DTOs;
+using KBlog.Models;
+using KBlog.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBlogTest.Controllers
+{
+	public class UserControllerTest
+	{
+		// private readonly Mock<IUserService> _userServiceMock;
+		private readonly UserController _controller;
+		private readonly Mock<IAuthService> _authServiceMock;
+		private readonly KBlogDbContext _dbContext;
+
+		public UserControllerTest()
+		{
+			// Sử dụng InMemoryDatabase để giả lập database
+			var options = new DbContextOptionsBuilder<KBlogDbContext>()
+							.UseInMemoryDatabase(databaseName: "KBlogTestDb")
+							.Options;
+
+			_dbContext = new KBlogDbContext(options);
+			_authServiceMock = new Mock<IAuthService>();
+			_controller = new UserController(_dbContext, _authServiceMock.Object);
+		}
+
+		// Test người dùng đăng ký thành công
+		[Fact]
+		public async Task Register_Success_ReturnOk()
+		{
+			// Arrange
+			var request = new RegisterRequest
+			{
+				UserName = "testuser",
+				Email = "testuser@example.com",
+				Password = "Test@123",
+			};
+			_authServiceMock.Setup(a => a.GenerateJwtToken(It.IsAny<User>(),
+															It.IsAny<string>(),
+															It.IsAny<string>()))
+															.Returns("mock-jwt-token");
+			
+			// Act
+			var result = await _controller.Register(request);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			Assert.Equal(200, okResult.StatusCode);
+		}
+	}
+}
---
+++ KBlogTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace KBlogTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
